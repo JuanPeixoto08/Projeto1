0bd440e77881bb69751c922164ab394e4483f566
JuanPeixoto08 <juanpeixoto1214@gmail.com>
2025-11-13 13:13:27 -0300

    Reajuste

--- form_produtos.php
+++
@@ -1,9 +0,0 @@
-<form action="?pg=cadastra_produtos" method="post">
-    <label>Nome do Produto:</label>
-    <input type="text" name="nome">
-    <label>Preço:</label>
-    <input type="text" name="preco">
-    <label>Descrição:</label>
-    <input type="text" name="descricao">
-    <input type="submit" value="Cadastrar">
-</form>
--- index.php
+++ index.php
@@ -1,22 +1,17 @@
 <?php
-    echo "Painel administrativo";
-?>
 
-<nav>
-    <a href="index.php">Inicio</a>
-    <a href="?pg=admin_clientes">Clientes</a>
-    <a href="?pg=admin_produtos">Produtos</a>
-    <a href="?pg=admin_paginas">Administra Páginas</a>
-    <a href="?pg=admin_contato">Administrar Contatos</a>
-</nav>
-<?php
+    include_once "topo.php";
+    include_once "menu.php";
+
     // Conteúdo
     if(empty($_SERVER["QUERY_STRING"])){
-       echo "<h3>Bem-vindo ao painel admin.";
+        $var = "conteudo";
+        include_once "$var.php";
     }elseif($_GET['pg']){
         $pg = $_GET['pg'];
         include_once "$pg.php";
     }else{
         echo "Página não encontrada";
     }
-?>
+
+    include_once "rodape.php";
